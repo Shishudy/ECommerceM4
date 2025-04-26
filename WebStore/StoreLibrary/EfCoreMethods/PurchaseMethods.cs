0e9992542db9ae55adfea87ba8af657b5aa6a133
Fandre-b <filipe.antunes94@gmail.com>
2025-04-26 20:16:08 +0100

    major save Filipe

--- WebStore/StoreLibrary/EfCoreMethods/PurchaseMethods.cs
+++ WebStore/StoreLibrary/EfCoreMethods/PurchaseMethods.cs
@@ -48,9 +48,13 @@ namespace StoreLibrary.EfCoreMethods
             }
         }
 
+
+
+
         // -------------------------------
-        // Cart Management
+        // Purchase Management
         // -------------------------------
+
         public Purchase GetCartByUserID(string fk_user)
         {
             var cart = _context.Purchases.FirstOrDefault(p => p.FkUser == fk_user && p.Status == "Cart");
@@ -67,25 +71,46 @@ namespace StoreLibrary.EfCoreMethods
             return cart;
         }
 
-        public List<PurchaseProduct> GetCartInfoByUserID(string fk_user)
+		public void DeletePurchase(string fk_user)
+		{
+			var cart = GetCartByUserID(fk_user);
+			if (cart != null)
+			{
+				DeleteAddressById(cart.FkAddressShipment);
+				DeleteCardById(cart.FkCard);
+				DeleteInvoice(cart.FkUser);
+				_context.PurchaseProducts.RemoveRange(_context.PurchaseProducts.Where(p => p.FkPurchase == cart.PkPurchase));
+				_context.Purchases.Remove(cart);
+				_context.SaveChanges();
+			}
+			else
+				throw new Exception("Cart not found");
+		}
+
+		// -------------------------------
+        // Cart Management
+        // -------------------------------
+		// Get - return, creates if nonexistent
+		// Post - create new #
+		// Put 	- update, upsert: create or update
+		// Delete - remove 
+		// -------------------------------
+
+        public List<PurchaseProduct> GetCartItemsByUserID(string fk_user)
         {
-            var cart = _context.PurchaseProducts.Where(p => p.FkPurchase == GetCartByUserID(fk_user).PkPurchase).ToList();
-            return cart;
+			Purchase cart = GetCartByUserID(fk_user);
+            List<PurchaseProduct> items = _context.PurchaseProducts.Where(p => p.FkPurchase == cart.PkPurchase).ToList();
+            return items;
         }
 
-        public void AddItemToCart(string fk_user, int productId, int quantity)
+		public void AddItemToCart(string fk_user, int productId, int quantity)
         {
             var cart = GetCartByUserID(fk_user);
 
             var existingItem = _context.PurchaseProducts
                 .FirstOrDefault(pp => pp.FkPurchase == cart.PkPurchase && pp.FkProduct == productId);
 
-            if (existingItem != null)
-            {
-                existingItem.Qtt += quantity;
-                _context.PurchaseProducts.Update(existingItem);
-            }
-            else
+            if (existingItem == null)
             {
                 var newItem = new PurchaseProduct
                 {
@@ -95,7 +120,14 @@ namespace StoreLibrary.EfCoreMethods
                 };
                 _context.PurchaseProducts.Add(newItem);
             }
-
+            else
+            {
+                existingItem.Qtt += quantity;
+				if (existingItem.Qtt < 1)
+					_context.PurchaseProducts.Remove(existingItem);
+				else
+                	_context.PurchaseProducts.Update(existingItem);
+            }
             _context.SaveChanges();
         }
 
@@ -113,15 +145,137 @@ namespace StoreLibrary.EfCoreMethods
             }
         }
 
+		// -------------------------------
+        // Card Management
         // -------------------------------
-        // Address Management
+		// Get - return #
+		// Post - create new
+		// Put 	- update #
+		// Delete - remove
+		// -------------------------------
+
+		public List<Card> GetCardsByUserID(string fk_user)
+		{
+			var cards = _context.Cards.Where(c => c.FkUser == fk_user && c.Toogle == true).ToList();
+			return cards;
+		}
+
+        public void AddCardToPurchase(string fk_user, Card card)
+        {
+            var cart = GetCartByUserID(fk_user);
+
+            var existingCard = _context.Cards
+                .FirstOrDefault(c => c.PkCard == cart.FkCard);
+
+			card.FkUser = fk_user;
+
+            if (existingCard == null)
+            {
+				cart.FkCardNavigation = card;
+                _context.Cards.Add(card);
+                // cart.FkCard = card.PkCard; // Uncomment if needed
+            }
+            else
+            {
+                existingCard.AssignFrom(card); // Copy values from the new card
+                _context.Cards.Update(existingCard);
+            }
+            _context.SaveChanges();
+        }
+
+        public void DeleteCardById(int? cardId)
+        {
+			if (cardId == null)
+				throw new Exception("Card ID is null");
+            var card = _context.Cards.FirstOrDefault(c => c.PkCard == cardId);
+            if (card != null)
+            {
+                card.Toogle = true; // Soft delete
+                _context.SaveChanges();
+            }
+			else
+				throw new Exception("Card not found");
+        }
+
+
+		// -------------------------------
+        // Invoice Management
         // -------------------------------
-        public void AddAddressToPurchase(string fk_user, Address address)
+		// Get - return #
+		// Post - create new
+		// Put 	- update #
+		// Delete - remove
+		// -------------------------------
+
+		public List<Invoice>? GetInvoicesByUserID(string fk_user)
+		{
+			IQueryable<int?> fkInvoices = _context.Purchases.Where(p => p.FkUser == fk_user).Select(p => p.FkInvoice);
+			var invoices = _context.Invoices.Where(i => fkInvoices.Contains(i.PkInvoice)).ToList();
+			return invoices;
+		}
+
+		public Invoice? GetInvoiceById(int id)
+		{
+			return _context.Invoices.FirstOrDefault(i => i.PkInvoice == id);
+		}
+
+        public void AddInvoiceToPurchase(string fk_user, Invoice invoice)
+        {
+            var cart = GetCartByUserID(fk_user);
+
+			cart.Status = "Pending Payment";
+            var existingInvoice = _context.Invoices
+                .FirstOrDefault(i => i.PkInvoice == cart.FkInvoice);
+
+            if (existingInvoice == null)
+            {
+				cart.FkInvoiceNavigation = invoice;
+                _context.Invoices.Add(invoice);
+                // cart.FkInvoice = invoice.PkInvoice; // Uncomment if needed
+            }
+            else
+            {
+                existingInvoice.AssignFrom(invoice); // Copy values from the new invoice
+                _context.Invoices.Update(existingInvoice);
+            }
+
+            _context.SaveChanges();
+        }
+
+		public void DeleteInvoice(string fk_user)
+		{
+			var cart = GetCartByUserID(fk_user);
+
+            var existingInvoice = _context.Invoices
+                .FirstOrDefault(i => i.PkInvoice == cart.FkInvoice);
+			if (existingInvoice != null && cart.Status != "finalized")
+			{
+				DeleteAddressById(existingInvoice.FkAddressInvoice);
+				_context.Invoices.Remove(existingInvoice);
+				_context.SaveChanges();
+			}
+			else
+				throw new Exception("Invoice not found or already finalized");
+		}
+
+        // -------------------------------
+        // Address Shipment Management
+        // -------------------------------
+
+		public List<Address> GetAddressesByUserID(string fk_user)
+		{
+			var addresses = _context.Addresses.Where(a => a.FkUser == fk_user && a.Toggle == true).ToList();
+			return addresses;
+		}
+
+		public void AddAddressToPurchase(string fk_user, Address address)
         {
             var cart = GetCartByUserID(fk_user);
 
             var existingAddress = _context.Addresses
                 .FirstOrDefault(a => a.PkAddress == cart.FkAddressShipment);
+
+			address.FkUser = fk_user;
 
             if (existingAddress == null)
             {
@@ -134,85 +288,33 @@ namespace StoreLibrary.EfCoreMethods
             else
             {
 				// if address in cart is not null, update existing address
-                existingAddress.Street = address.Street;
-                existingAddress.City = address.City;
-                existingAddress.State = address.State;
-                existingAddress.ZipCode = address.ZipCode;
-                existingAddress.Country = address.Country;
-                _context.Addresses.Update(existingAddress);
+                existingAddress.AssignFrom(address); // Copy values from the new address
+				_context.Addresses.Update(existingAddress);
             }
 
             _context.SaveChanges();
         }
 
-        public void DeleteAddressById(int addressId)
-        {
-            var address = _context.Addresses.FirstOrDefault(a => a.PkAddress == addressId);
-            if (address != null)
-            {
-                address.Toggle = true; // Soft delete
-                _context.SaveChanges();
-            }
-			else 
-				new Exception("Address not found");
-        }
 
-
-
-1
-
-		// 	// Save changes to persist the updates
-		// 	_context.SaveChanges();
-		// }
-
+		// -------------------------------
+        // Address Invoice Management
         // -------------------------------
-        // Card Management
-        // -------------------------------
-        public void AddCardToPurchase(string fk_user, Card card)
-        {
-            var cart = GetCartByUserID(fk_user);
 
-            var existingCard = _context.Cards
-                .FirstOrDefault(c => c.PkCard == cart.FkCard);
-
-            if (existingCard == null)
-            {
-				cart.FkCardNavigation = card;
-                _context.Cards.Add(card);
-                // cart.FkCard = card.PkCard; // Uncomment if needed
-            }
-            else
-            {
-                existingCard.CardType = card.CardType;
-                existingCard.CardHolder = card.CardHolder;
-                existingCard.CardNumber = card.CardNumber;
-                existingCard.Cvv = card.Cvv;
-                existingCard.ExpirationDate = card.ExpirationDate;
-                _context.Cards.Update(existingCard);
-            }
-
-            _context.SaveChanges();
-        }
-
-        public void DeleteCardById(int cardId)
-        {
-            var card = _context.Cards.FirstOrDefault(c => c.PkCard == cardId);
-            if (card != null)
-            {
-                card.Toogle = true; // Soft delete
-                _context.SaveChanges();
-            }
-        }
-
-        // -------------------------------
-        // Invoice Management
-        // -------------------------------
+		public List<Address> GetInvoiceAddressByUser(string fk_user)
+		{
+			var invoices = GetInvoicesByUserID(fk_user)
+				.Where(i => i.FkAddressInvoice != null )
+				.Select(i => i.FkAddressInvoiceNavigation);
+			return invoices.Where(a => a.Toggle == true).ToList();
+		}
 
 		public void AddAddressToInvoice(string fk_user, Address address)
 		{
 			var cart = GetCartByUserID(fk_user);
 			var existingInvoice = _context.Invoices
                 .FirstOrDefault(i => i.PkInvoice == cart.FkInvoice);
+
+			// address.FkUser = fk_user;
 
 			if (existingInvoice.FkAddressInvoice == null)
 			{
@@ -228,45 +330,41 @@ namespace StoreLibrary.EfCoreMethods
 
 				if (existingAddress != null)
 				{
-					existingAddress.Street = address.Street;
-					existingAddress.City = address.City;
-					existingAddress.State = address.State;
-					existingAddress.ZipCode = address.ZipCode;
-					existingAddress.Country = address.Country;
+					existingAddress.AssignFrom(address); // Copy values from the new address
 					_context.Addresses.Update(existingAddress);
 				}
+				else
+					throw new Exception("Address not found");
 			}
 			_context.SaveChanges();
 		}
 
-		// public void DeleteAddressFromInvoice(string fk_user, int addressId)
-
-        public void AddInvoiceToPurchase(string fk_user, Address address, Invoice invoice)
+        public void DeleteAddressById(int? addressId)
         {
-            var cart = GetCartByUserID(fk_user);
-
-            var existingInvoice = _context.Invoices
-                .FirstOrDefault(i => i.PkInvoice == cart.FkInvoice);
-
-            if (existingInvoice == null)
+			if (addressId == null)
+				throw new Exception("Address ID is null");
+            var address = _context.Addresses.FirstOrDefault(a => a.PkAddress == addressId);
+            if (address != null)
             {
-				cart.FkInvoiceNavigation = invoice;
-                _context.Invoices.Add(invoice);
-                // cart.FkInvoice = invoice.PkInvoice; // Uncomment if needed
+                address.Toggle = true; // Soft delete
+                _context.SaveChanges();
             }
-            else
-            {
-                existingInvoice.InvoiceType = invoice.InvoiceType;
-                existingInvoice.InvoiceStatus = invoice.InvoiceStatus;
-                existingInvoice.InvoiceNumber = invoice.InvoiceNumber;
-                existingInvoice.InvoiceDate = invoice.InvoiceDate;
-                existingInvoice.TotalAmount = invoice.TotalAmount;
-
-
-                _context.Invoices.Update(existingInvoice);
-            }
-
-            _context.SaveChanges();
+			else 
+				new Exception("Address not found");
         }
+
+		// -------------------------------
+		// Invoice Management
+		// -------------------------------
+
+		// -------------------------------
+		// Invoice Management
+		// -------------------------------
+
+		// -------------------------------
+        // Cart Management
+        // -------------------------------
+
+
     }
 }
