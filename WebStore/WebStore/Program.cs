0a62bcbaf64c5a8e552f7c92e9de902c324fc96f
HapyLen <leandroaurelio98@gmail.com>
2025-04-29 01:10:30 +0100

    Pagina demo AddProducts

--- WebStore/WebStore/Program.cs
+++ WebStore/WebStore/Program.cs
@@ -37,6 +37,8 @@ builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<FavouriteService>();
 builder.Services.AddScoped<CategoryService>();
 builder.Services.AddScoped<ImageService>();
+builder.Services.AddScoped<AddProductService>();
+
 
 var app = builder.Build();
 
